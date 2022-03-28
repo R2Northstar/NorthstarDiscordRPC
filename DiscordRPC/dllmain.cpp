e93378c77d73466d44aae9821be9c9c1039c2361
Emma Miler <27428383+emma-miler@users.noreply.github.com>
2022-03-28 22:15:02 +0200

    Remove debug print oops

--- DiscordRPC/dllmain.cpp
+++ DiscordRPC/dllmain.cpp
@@ -233,7 +233,6 @@ int main(int, char**)
 				details += " (First to " + std::to_string(pluginData.scoreLimit) + ")";
 				activity.SetDetails(details.c_str());
 				const auto p1 = std::chrono::system_clock::now().time_since_epoch();
-				printf("%i\n", pluginData.endTime);
 				if (pluginData.endTime > 0) {
 					activity.GetTimestamps().SetEnd(std::chrono::duration_cast<std::chrono::seconds>(p1).count() + pluginData.endTime);
 					activity.GetTimestamps().SetStart(0);
