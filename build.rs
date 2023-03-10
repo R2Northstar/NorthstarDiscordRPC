61a07b2d09afef4afd1ba8f16104fecd7c803893
cat_or_not <41955154+catornot@users.noreply.github.com>
2023-03-10 18:31:17 -0500

    v2 rewrite

--- build.rs
+++ build.rs
@@ -2,10 +2,5 @@ extern crate windres;
 use windres::Build;
 
 fn main() {
-    Build::new().compile("manifest\\Resource.rc").unwrap();
-
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=Cargo.lock");
-    println!("cargo:rerun-if-changed=r2rsplugins\\headers\\Resource.rc");
-    println!("cargo:rerun-if-changed=r2rsplugins\\manifest.json");
+    Build::new().compile("manifest/Resource.rc").expect("failed to include ressouces in the dll");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,16 @@
-use std::{time::{SystemTime, UNIX_EPOCH}, sync::{Mutex, mpsc::{Receiver, channel}}};
+use std::{
+    sync::Mutex,
+    time::{SystemTime, UNIX_EPOCH},
+};
 
-use discord_presence::Client;
+use discord_game_sdk::{Activity, Discord, EventHandler};
 use rrplug::prelude::*;
+use rrplug::wrappers::presence::{GamePresence, GameStateEnum};
 
 const EMPTY: String = String::new();
+const APP_ID: i64 = 941428101429231617;
 
-static mut WAS_IN_GAME: bool = false;
-static mut RESET_SINGLE_PLAYER_TIMER: bool = true;
-
+#[derive(Debug, Default, Clone)]
 struct ActivityData {
     party: (u32, u32),
     details: String,
@@ -16,246 +19,178 @@ struct ActivityData {
     large_text: String,
     small_image: String,
     small_text: String,
-    end: Option<u64>,
-    start: Option<u64>,
+    end: i64,
+    start: i64,
 }
 
+#[derive(Debug)]
 struct DiscordRpc {
-    gamestate: Option<GameState>,
-    serverinfo: Option<ServerInfo>,
-    drpc: Option<Mutex<Client>>,
-    recv: Option<Receiver<bool>>
+    activity: Option<Mutex<ActivityData>>,
 }
 
 impl Plugin for DiscordRpc {
     fn new() -> Self {
-        Self {
-            gamestate: None,
-            serverinfo: None,
-            drpc: None,
-            recv: None
-        }
+        Self { activity: None }
     }
 
-    fn initialize(&mut self, external_plugin_data: ExternalPluginData) {
-        self.gamestate = external_plugin_data.get_game_state_struct();
-        self.serverinfo = external_plugin_data.get_server_info_struct();
-        println!("[DiscordRPC] discord rpc plugin initialized");
-
-        let mut drpc = Client::new(941428101429231617);
-        
-        let _ = drpc.start();
-
-        let (send, recv) = channel::<bool>();
-        self.recv = Some(recv);
-        let send = Mutex::new(send);
-
-        // drpc.block_until_event(discord_presence::Event::Ready).unwrap(); // hmm doesn't work?
-
-        drpc.on_ready(move |_| send.lock().unwrap().send(true).unwrap());
-
-        self.drpc = Some(Mutex::new(drpc));
+    fn initialize(&mut self, _: &PluginData) {
+        self.activity = Some(Mutex::new(ActivityData {
+            large_image: "northstar".to_string(),
+            state: "Loading...".to_string(),
+            ..Default::default()
+        }));
     }
 
     fn main(&self) {
-        let gamestate = self.gamestate.as_ref().unwrap();
-        let serverinfo = self.serverinfo.as_ref().unwrap();
-        let mut drpc = self.drpc.as_ref().unwrap().lock().unwrap();
-        let recv = self.recv.as_ref().unwrap();
-        
-        println!("[DiscordRPC] waiting for discord rpc to be ready");
-        loop {
-            if recv.try_recv().is_ok() {
-                println!("[DiscordRPC] discord rpc initialized");
-                break;
+        let mut drpc: Discord<'_, DiscordEvent> = match Discord::new(APP_ID) {
+            Ok(drpc) => drpc,
+            Err(err) => {
+                log::error!("failed to load discord rpc; Is your discord running?");
+                log::error!("{err}");
+                return;
             }
+        };
+
+        drpc.clear_activity(|discord, _| {
+            discord.update_activity(
+                Activity::empty()
+                    .with_state("Loading...")
+                    .with_small_image_key("northstar"),
+                |_, result| match result {
+                    Ok(_) => log::info!("cleared activity"),
+                    Err(err) => log::error!("coudln't clear activity because of {:?}", err),
+                },
+            )
+        });
+
+        if let Err(err) = drpc.run_callbacks() {
+            #[cfg(debug_assertions)]
+            log::error!("failed to run callbacks {err}");
+            #[cfg(not(debug_assertions))]
+            drop(err)
+        }
+
+        loop {
+            let data = self.activity.as_ref().unwrap().lock().unwrap().clone();
+
+            drpc.update_activity(
+                Activity::empty()
+                    .with_details(&data.details)
+                    .with_state(&data.state)
+                    .with_large_image_key(&data.large_image)
+                    .with_small_image_key(&data.small_image)
+                    .with_large_image_tooltip(&data.large_text)
+                    .with_small_image_tooltip(&data.small_text)
+                    .with_end_time(data.end)
+                    .with_start_time(data.start)
+                    .with_party_amount(data.party.0)
+                    .with_party_capacity(data.party.1),
+                |_, result| {
+                    if let Err(err) = result {
+                        #[cfg(debug_assertions)]
+                        log::error!("coudln't update activity because of {:?}", err)
+                    }
+                },
+            );
+
+            if let Err(err) = drpc.run_callbacks() {
+                #[cfg(debug_assertions)]
+                log::error!("failed to run callbacks {err}");
+                #[cfg(not(debug_assertions))]
+                drop(err)
+            }
+
             wait(1000)
         }
+    }
 
-        loop {
-            if let Err(err) = drpc.clear_activity() {
-                #[cfg(debug_assertions)]
-                println!("[DiscordRPC] coudln't clear activity because of {:?}", err)
+    fn on_presence_updated(&self, presence: &GamePresence) {
+        let mut activity = match self.activity.as_ref().unwrap().try_lock() {
+            Ok(a) => a,
+            Err(_) => return,
+        };
+
+        match presence.get_state().unwrap() {
+            GameStateEnum::InGame => {}
+            _ => {
+                let start = SystemTime::now();
+                let since_the_epoch = start.duration_since(UNIX_EPOCH).unwrap(); // there is no way this fails
+                activity.start = since_the_epoch.as_secs() as i64;
             }
-            
-            #[cfg(debug_assertions)]
-            println!("[DiscordRPC] started a cycle");
+        };
 
-            let playlist = gamestate.playlist();
-            let playlist_display_name = gamestate.playlist_display_name();
-            let map = gamestate.map();
-            let map_display_name = gamestate.map_display_name();
-            let loading = gamestate.loading();
-            let players = gamestate.players() as u32;
-            let max_players = serverinfo.max_players() as u32;
+        match presence.get_state().unwrap() {
+            GameStateEnum::Loading => {
+                activity.party = (0, 0);
+                activity.details = "".to_string();
+                activity.state = "Loading...".to_string();
+                activity.large_image = "northstar".to_string();
+                activity.large_text = "Titanfall 2 + Northstar".to_string();
+                activity.small_image = EMPTY;
+                activity.small_text = EMPTY;
+                activity.end = 0;
+            }
+            GameStateEnum::MainMenu => {
+                activity.party = (0, 0);
+                activity.details = "Main Menu".to_string();
+                activity.state = "On Main Menu".to_string();
+                activity.large_image = "northstar".to_string();
+                activity.large_text = "Titanfall 2 + Northstar".to_string();
+                activity.small_image = EMPTY;
+                activity.small_text = EMPTY;
+                activity.end = 0;
+            }
+            GameStateEnum::Lobby => {
+                activity.party = (0, 0);
+                activity.details = "Lobby".to_string();
+                activity.state = "In the Lobby".to_string();
+                activity.large_image = "northstar".to_string();
+                activity.large_text = "Titanfall 2 + Northstar".to_string();
+                activity.small_image = EMPTY;
+                activity.small_text = EMPTY;
+                activity.end = 0;
+            }
+            GameStateEnum::InGame => {
+                let map_displayname = presence.get_map_displayname();
 
-            let mut data = ActivityData {
-                party: (0, 0),
-                details: EMPTY,
-                state: EMPTY,
-                large_image: EMPTY,
-                large_text: EMPTY,
-                small_image: EMPTY,
-                small_text: EMPTY,
-                end: None,
-                start: None,
-            };
-
-            if map.is_empty() {
-                data = ActivityData {
-                    party: (0, 0),
-                    details: "Main Menu".to_string(),
-                    state: "On Main Menu".to_string(),
-                    large_image: "northstar".to_string(),
-                    large_text: "Titanfall 2 + Northstar".to_string(),
-                    small_image: EMPTY,
-                    small_text: EMPTY,
-                    end: Some(0),
-                    start: None,
-                };
-
-                if get_was_in_game() {
-                    reset_timer(&mut data);
-
-                    set_was_in_game(false);
-                    set_reset_single_player_timer(true);
-                }
-            } else if map.starts_with("mp_lobby") {
-                data = ActivityData {
-                    party: (0, 0),
-                    details: "Lobby".to_string(),
-                    state: "In the Lobby".to_string(),
-                    large_image: "northstar".to_string(),
-                    large_text: "Titanfall 2 + Northstar".to_string(),
-                    small_image: EMPTY,
-                    small_text: EMPTY,
-                    end: Some(0),
-                    start: None,
-                };
-
-                if get_was_in_game() {
-                    reset_timer(&mut data);
-
-                    set_was_in_game(false);
-                    set_reset_single_player_timer(true);
-                }
-            } else if loading {
-                data.party = (players, max_players);
-                data.details = "Loading...".to_string();
-                if get_was_in_game() {
-                    set_was_in_game(false);
-                    set_reset_single_player_timer(true);
-                }
-            } else {
-                data.party = (players, max_players);
-
-                if playlist.starts_with("Campaign") {
-                    data = ActivityData {
-                        party: (0, 0),
-                        details: map_display_name,
-                        state: playlist_display_name,
-                        large_image: EMPTY,
-                        large_text: EMPTY,
-                        small_image: EMPTY,
-                        small_text: EMPTY,
-                        end: Some(0),
-                        start: None,
-                    };
-
-                    if get_reset_single_player_timer() {
-                        reset_timer(&mut data);
-
-                        set_was_in_game(false);
-                        set_reset_single_player_timer(true);
-                    }
+                activity.party = (
+                    presence
+                        .get_current_players()
+                        .try_into()
+                        .unwrap_or_default(),
+                    presence.get_max_players().try_into().unwrap_or_default(),
+                );
+                activity.details = map_displayname.clone();
+                activity.state = map_displayname.clone();
+                activity.large_image = presence.get_map();
+                activity.large_text = map_displayname;
+                if presence.get_playlist() == "campaign" {
+                    activity.party = (0, 0);
+                    activity.end = 0;
                 } else {
-                    let our_score = gamestate.our_score();
-                    let second_highest_score = gamestate.second_highest_score();
-                    let highest_score = gamestate.highest_score();
-                    let score_limit = serverinfo.score_limit();
-                    let endtime = serverinfo.end_time() as u64;
+                    activity.state = presence.get_playlist_displayname().replace("#PL_", "");
+                    activity.details = format!(
+                        "Score: {} - {} (First to {})",
+                        presence.get_own_score(),
+                        presence.get_other_highest_score(),
+                        presence.get_max_score()
+                    );
 
-                    data.details = if our_score == highest_score {
-                        format!("{} - {}", our_score, second_highest_score)
-                    } else {
-                        format!("{} - {}", our_score, highest_score)
-                    };
-
-                    data.details
-                        .push_str(&format!(" (First to {})", score_limit)[..]);
-                    if endtime > 0 {
-                        let start = SystemTime::now();
-                        let since_the_epoch = start.duration_since(UNIX_EPOCH).unwrap();
-
-                        data.end = Some(since_the_epoch.as_secs() + endtime);
-                        data.start = Some(0);
-                    }
-                    set_reset_single_player_timer(false);
+                    let current_time = SystemTime::now()
+                        .duration_since(UNIX_EPOCH)
+                        .unwrap()
+                        .as_secs() as i64;
+                    let ig_end = presence.get_timestamp_end() as i64;
+                    activity.end = current_time + ig_end;
                 }
-                set_was_in_game(true);
             }
-
-            println!("[DiscordRPC] setting the activity");
-            if let Err(err) = drpc.set_activity(|act| {
-                act.party(|party| party.size(data.party))
-                    .details(data.details)
-                    .state(data.state)
-                    .assets(|asset| {
-                        asset
-                            .large_image(data.large_image)
-                            .small_image(data.small_image)
-                            .large_text(data.large_text)
-                            .small_text(data.small_text)
-                    })
-                    .timestamps(|time| {
-                        let time = if data.end.is_some() {
-                            time.end(data.end.unwrap())
-                        } else {
-                            time
-                        };
-
-                        if data.start.is_some() {
-                            time.start(data.start.unwrap())
-                        } else {
-                            time
-                        }
-                    })
-            }) {
-                #[cfg(debug_assertions)]
-                println!("[DiscordRPC] coudln't set activity because of {:?}", err)
-            }
-            
-            #[cfg(debug_assertions)]
-            println!("[DiscordRPC] completed a cycle");
-            wait(100)
-        }
+        };
     }
 }
 
 entry!(DiscordRpc);
 
-fn reset_timer(data: &mut ActivityData) {
-    let start = SystemTime::now();
-    let since_the_epoch = start.duration_since(UNIX_EPOCH).unwrap(); // there is no way this fails
+#[derive(Default)]
+pub struct DiscordEvent;
 
-    let in_ms =
-        since_the_epoch.as_secs() * 1000 + since_the_epoch.subsec_nanos() as u64 / 1_000_000;
-
-    data.start = Some(in_ms);
-}
-
-fn get_was_in_game() -> bool {
-    unsafe { WAS_IN_GAME }
-}
-
-fn set_was_in_game(wig: bool) {
-    unsafe { WAS_IN_GAME = wig }
-}
-
-fn get_reset_single_player_timer() -> bool {
-    unsafe { RESET_SINGLE_PLAYER_TIMER }
-}
-
-fn set_reset_single_player_timer(val: bool) {
-    unsafe { RESET_SINGLE_PLAYER_TIMER = val }
-}
+impl EventHandler for DiscordEvent {}
